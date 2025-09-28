032c1d00c3a2e101f2b58d9a6d4ead7c5dc928bf
Lavish Thakkar <lavisht22@gmail.com>
2025-09-28 13:27:07 +0530

    Installed Hero UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import logo from '../logo.svg'
+import { Button } from '@heroui/react'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -8,6 +9,7 @@ export const Route = createFileRoute('/')({
 function App() {
   return (
     <div className="text-center">
+      <Button color='primary'>HeroUI!</Button>
       <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
         <img
           src={logo}
