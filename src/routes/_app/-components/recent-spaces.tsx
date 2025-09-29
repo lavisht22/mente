faf6caf5de88ba5b2f88d09aa0752edeab300f5f
Lavish Thakkar <lavisht22@gmail.com>
2025-09-29 17:53:25 +0530

    Recent spaces section

--- src/routes/_app/-components/recent-spaces.tsx
+++ src/routes/_app/-components/recent-spaces.tsx
@@ -1,3 +1,42 @@
+import Space from "@/components/space";
+import { spacesQuery } from "@/lib/queries";
+import { Alert, Skeleton } from "@heroui/react";
+import { useQuery } from "@tanstack/react-query";
+
+function SpaceSkeleton() {
+	return <Skeleton className="w-[180px] h-[216px] shrink-0 rounded-large" />;
+}
+
 export default function RecentSpaces() {
-	return null;
+	const { data, isLoading, isError } = useQuery(spacesQuery);
+
+	if (isLoading) {
+		return (
+			<div className="flex gap-4 w-full overflow-x-auto p-8 -mt-8 scrollbar-hide">
+				<SpaceSkeleton />
+				<SpaceSkeleton />
+				<SpaceSkeleton />
+			</div>
+		);
+	}
+
+	if (isError) {
+		return (
+			<div className="p-8 -mt-8 flex items-center justify-center h-[216px]">
+				<Alert
+					className="max-w-md"
+					color="danger"
+					title="There was an error loading recent spaces."
+				/>
+			</div>
+		);
+	}
+
+	return (
+		<div className="flex gap-4 w-full overflow-x-auto p-8 -mt-8 scrollbar-hide">
+			{data?.map((item) => (
+				<Space key={item.id} {...item} />
+			))}
+		</div>
+	);
 }
