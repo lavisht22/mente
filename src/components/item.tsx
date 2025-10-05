c263af2bf38460fa57e57b24721e0758004533f1
Lavish Thakkar <lavisht22@gmail.com>
2025-10-05 11:49:27 +0000

    Better markdown preview

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -1,26 +1,29 @@
-import type { Tables } from "@/lib/supabase.types";
 import { Card, CardBody } from "@heroui/react";
 import { Link } from "@tanstack/react-router";
+import type { Tables } from "db.types";
+import removeMd from "remove-markdown";
 
 export default function Item({ id, title, type, markdown }: Tables<"items">) {
-	if (type === "note") {
-		return (
-			<Card
-				as={Link}
-				to={`/items/${id}`}
-				isPressable
-				className="w-[180px] h-[216px] shrink-0 bg-yellow-50"
-				shadow="sm"
-			>
-				<CardBody className="hover:bg-default/20 flex flex-col gap-2">
-					<p className="text-sm text-default-500 font-medium line-clamp-1">
-						{title}
-					</p>
-					<p className="text-xs text-default-500 line-clamp-5">{markdown}</p>
-				</CardBody>
-			</Card>
-		);
-	}
+  if (type === "note") {
+    const plainText = markdown ? removeMd(markdown) : "";
 
-	return null;
+    return (
+      <Card
+        as={Link}
+        to={`/items/${id}`}
+        isPressable
+        className="w-[180px] h-[216px] shrink-0 bg-yellow-50"
+        shadow="sm"
+      >
+        <CardBody className="hover:bg-default/20 flex flex-col gap-2">
+          <p className="text-sm text-default-500 font-medium line-clamp-1">
+            {title}
+          </p>
+          <p className="text-xs text-default-500 line-clamp-5">{plainText}</p>
+        </CardBody>
+      </Card>
+    );
+  }
+
+  return null;
 }
--- src/routes/_app/index.tsx
+++ src/routes/_app/index.tsx
@@ -2,40 +2,52 @@ import NewDropdown from "@/components/new-dropdown";
 import { Button } from "@heroui/react";
 import { createFileRoute } from "@tanstack/react-router";
 import { LucidePlus } from "lucide-react";
+import { useCallback } from "react";
 import RecentItems from "./-components/recent-items";
 import RecentSpaces from "./-components/recent-spaces";
 
 export const Route = createFileRoute("/_app/")({
-	component: RouteComponent,
+  component: RouteComponent,
 });
 
 function RouteComponent() {
-	return (
-		<div>
-			<div className="flex items-center gap-4 p-8">
-				<div className="flex-1">
-					<h1 className="text-2xl font-bold">Greeting!</h1>
-				</div>
-				<div>
-					<NewDropdown
-						trigger={
-							<Button isIconOnly color="primary" variant="flat">
-								<LucidePlus className="size-5" />
-							</Button>
-						}
-					/>
-				</div>
-			</div>
+  const getGreeting = useCallback(() => {
+    const hour = new Date().getHours();
+    if (hour < 12) {
+      return "Good morning";
+    }
+    if (hour < 18) {
+      return "Good afternoon";
+    }
+    return "Good evening";
+  }, []);
 
-			<div className="space-y-4">
-				<h3 className="text-lg font-medium ml-8">Recent Items</h3>
-				<RecentItems />
-			</div>
+  return (
+    <div>
+      <div className="flex items-center gap-4 p-8">
+        <div className="flex-1">
+          <h1 className="text-4xl">{getGreeting()}</h1>
+        </div>
+        <div>
+          <NewDropdown
+            trigger={
+              <Button isIconOnly color="primary" variant="flat">
+                <LucidePlus className="size-5" />
+              </Button>
+            }
+          />
+        </div>
+      </div>
 
-			<div className="space-y-4">
-				<h3 className="text-lg font-medium ml-8">Recent Spaces</h3>
-				<RecentSpaces />
-			</div>
-		</div>
-	);
+      <div className="space-y-4">
+        <h3 className="text-lg font-medium ml-8">Recent Items</h3>
+        <RecentItems />
+      </div>
+
+      <div className="space-y-4">
+        <h3 className="text-lg font-medium ml-8">Recent Spaces</h3>
+        <RecentSpaces />
+      </div>
+    </div>
+  );
 }
