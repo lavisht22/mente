6ef7e77392b39a5fe25d0a20e9ac73df03ea071c
Lavish Thakkar <lavisht22@gmail.com>
2025-10-05 12:21:54 +0000

    Fix build

--- src/components/space.tsx
+++ src/components/space.tsx
@@ -1,21 +1,21 @@
-import type { Tables } from "@/lib/supabase.types";
 import { Card, CardBody } from "@heroui/react";
 import { Link } from "@tanstack/react-router";
+import type { Tables } from "db.types";
 
 export default function Item({ id, name }: Tables<"spaces">) {
-	return (
-		<Card
-			as={Link}
-			to={`/spaces/${id}`}
-			isPressable
-			className="w-[180px] h-[216px] shrink-0"
-			shadow="sm"
-		>
-			<CardBody className="hover:bg-default/20 flex flex-col gap-2">
-				<p className="text-sm text-default-500 font-medium line-clamp-1">
-					{name}
-				</p>
-			</CardBody>
-		</Card>
-	);
+  return (
+    <Card
+      as={Link}
+      to={`/spaces/${id}`}
+      isPressable
+      className="w-[180px] h-[216px] shrink-0"
+      shadow="sm"
+    >
+      <CardBody className="hover:bg-default/20 flex flex-col gap-2">
+        <p className="text-sm text-default-500 font-medium line-clamp-1">
+          {name}
+        </p>
+      </CardBody>
+    </Card>
+  );
 }
