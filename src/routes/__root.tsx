aeaa53c809a09fc94c7fd44b147b017c8caf8f68
Lavish Thakkar <lavisht22@gmail.com>
2025-10-06 11:27:14 +0000

    Remove additional prop

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,7 +12,6 @@ export const Route = createRootRouteWithContext<RouterContext>()({
     <>
       <Outlet />
       <TanstackDevtools
-        initialIsOpen={false}
         config={{
           position: "bottom-left",
         }}
