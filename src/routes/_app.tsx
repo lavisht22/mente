4fe07d0b90b0cd6b87e788023a168ea51006f405
Lavish Thakkar <lavisht22@gmail.com>
2025-11-19 13:12:43 +0530

    Hide nav on some pages

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -1,4 +1,9 @@
-import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
+import {
+  Outlet,
+  createFileRoute,
+  redirect,
+  useMatchRoute,
+} from "@tanstack/react-router";
 
 import Nav from "@/components/nav";
 import supabase from "@/lib/supabase";
@@ -20,10 +25,16 @@ export const Route = createFileRoute("/_app")({
 });
 
 function RouteComponent() {
+  const matchRoute = useMatchRoute();
+
+  const hideNav =
+    matchRoute({ to: "/chats/$id", fuzzy: false }) ||
+    matchRoute({ to: "/spaces/$spaceId/items/$itemId", fuzzy: false });
+
   return (
     <main className="h-[100dvh] w-[100dvw] overflow-hidden bg-default-50">
       <Outlet />
-      <Nav />
+      {!hideNav && <Nav />}
     </main>
   );
 }
